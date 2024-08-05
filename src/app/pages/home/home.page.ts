c570367fdad86cf3841eab177779636a5d61fb98
Wilfred <wilcr20@gmail.com>
2024-08-04 21:42:41 -0600

    Add setup to get data from local JSON and add more pokemons data

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import pokemonsData from "../../data/pokemons.json";
+
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+
+  pokemonsDataJSON: any = pokemonsData.data;
+
+  ionViewWillEnter() {
+    // let data = pokemonsData.data 
+    console.log("Pokemon : ", this.pokemonsDataJSON);
+
+  }
+
+  getImgSource(pokemon: any, sufix: string) {
+    return "../../../assets/images/pokemons/" + sufix;
+  }
+
+  playSound(sound: string) {
+    var audio = new Audio('../../../assets/sounds/pokemons/'+sound);
+    audio.volume = 0.2;
+    audio.play();
+  }
 
 }
 
