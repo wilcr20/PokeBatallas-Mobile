1d87805d7e2d39727fcf6436e59a1ba44a5a59b6
Wilfred <wilcr20@gmail.com>
2024-08-07 21:46:51 -0600

    Adding styling and fucntionality for Pokedex page (In progress)

--- src/app/pages/pokedex/pokedex.page.ts
+++ src/app/pages/pokedex/pokedex.page.ts
@@ -7,13 +7,25 @@ import pokemonsData from "../../data/pokemons.json";
 })
 export class PokedexPage {
 
+  pokemonSelected: any;
+  isModalOpen = false;
   pokemonsDataJSON: any = pokemonsData.data;
+  backgroundAudio : any;
+  backgroundSounds = [
+    "Hurry Along 1 - Pokémon Black & Pokémon White (OST).mp3",
+    "Hurry Along 2 - Pokémon Black & Pokémon White (OST).mp3"
+  ]
 
   constructor() { }
 
   ionViewWillEnter() {
     // let data = pokemonsData.data 
     console.log("Pokemon : ", this.pokemonsDataJSON);
+    let index = Math.floor(Math.random() * 2);   
+    this.backgroundAudio = new Audio('../../../assets/sounds/background/'+this.backgroundSounds[index]);
+    this.backgroundAudio.volume = 0.5;
+    this.backgroundAudio.loop = true;
+    this.backgroundAudio.play();
   }
 
   getImgSource(sufix: string) {
@@ -22,8 +34,21 @@ export class PokedexPage {
 
   playSound(sound: string) {
     var audio = new Audio('../../../assets/sounds/pokemons/'+sound);
-    audio.volume = 0.2;
+    audio.volume = 0.3;
     audio.play();
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  setPokemonSelected(pokemon:any){
+    this.pokemonSelected = null;
+    this.pokemonSelected = pokemon;
+  }
+
+  ionViewWillLeave(){
+    this.backgroundAudio.pause()
   }
 
 }
